f8c588c401533b56b18ada63db677f8764a75ef0
dj.quam@gmail.com <dj.quam@gmail.com@d7e7eb5c-c029-b68f-3db2-f32950ae7c21>
2011-12-13 03:46:26 +0000

    Adding OpenGL rendering glutInit and mainloop functionality

--- trunk/Subversion_Kinect/Subversion_Kinect/Subversion_Kinect/main.cpp
+++ trunk/Subversion_Kinect/Subversion_Kinect/Subversion_Kinect/main.cpp
@@ -127,9 +127,127 @@ void XN_CALLBACK_TYPE NoHands(void * UserCxt)
 	g_SessionState = QUICK_REFOCUS;
 }
 
+void XN_CALLBACK_TYPE TouchingCallback(xn::HandTouchingFOVEdgeCapability& generator, XnUserID id, const XnPoint3D* pPosition, XnFloat fTime, XnDirection eDir, void* pCookie)
+{
+	g_pDrawer->SetTouchingFOVEdge(id);
+}
 
 
+void glutDisplay (void)
+{
 
+	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Setup the OpenGL viewpoint
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+
+	XnMapOutputMode mode;
+	g_DepthGenerator.GetMapOutputMode(mode);
+#ifdef USE_GLUT
+	glOrtho(0, mode.nXRes, mode.nYRes, 0, -1.0, 1.0);
+#elif defined(USE_GLES)
+	glOrthof(0, mode.nXRes, mode.nYRes, 0, -1.0, 1.0);
+#endif
+
+	glDisable(GL_TEXTURE_2D);
+
+	if (!g_bPause)
+	{
+		// Read next available data
+		g_Context.WaitOneUpdateAll(g_DepthGenerator);
+		// Update NITE tree
+		g_pSessionManager->Update(&g_Context);
+#ifdef USE_GLUT
+		PrintSessionState(g_SessionState);
+#endif
+	}
+
+#ifdef USE_GLUT
+	glutSwapBuffers();
+#endif
+}
+
+#ifdef USE_GLUT
+void glutIdle (void)
+{
+	if (g_bQuit) {
+		CleanupExit();
+	}
+
+	// Display the frame
+	glutPostRedisplay();
+}
+
+void glutKeyboard (unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 27:
+		// Exit
+		CleanupExit();
+	case'p':
+		// Toggle pause
+		g_bPause = !g_bPause;
+		break;
+	case 'd':
+		// Toggle drawing of the depth map
+		g_bDrawDepthMap = !g_bDrawDepthMap;
+		g_pDrawer->SetDepthMap(g_bDrawDepthMap);
+		break;
+	case 'f':
+		g_bPrintFrameID = !g_bPrintFrameID;
+		g_pDrawer->SetFrameID(g_bPrintFrameID);
+		break;
+	case 's':
+		// Toggle smoothing
+		if (g_fSmoothing == 0)
+			g_fSmoothing = 0.1;
+		else 
+			g_fSmoothing = 0;
+		g_HandsGenerator.SetSmoothing(g_fSmoothing);
+		break;
+	case 'e':
+		// end current session
+		g_pSessionManager->EndSession();
+		break;
+	}
+}
+void glInit (int * pargc, char ** argv)
+{
+	glutInit(pargc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(GL_WIN_SIZE_X, GL_WIN_SIZE_Y);
+	glutCreateWindow ("PrimeSense Nite Point Viewer");
+	//glutFullScreen();
+	glutSetCursor(GLUT_CURSOR_NONE);
+
+	glutKeyboardFunc(glutKeyboard);
+	glutDisplayFunc(glutDisplay);
+	glutIdleFunc(glutIdle);
+
+	glDisable(GL_DEPTH_TEST);
+	glEnable(GL_TEXTURE_2D);
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glDisableClientState(GL_COLOR_ARRAY);
+}
+#endif
+
+
+void XN_CALLBACK_TYPE GestureIntermediateStageCompletedHandler(xn::GestureGenerator& generator, const XnChar* strGesture, const XnPoint3D* pPosition, void* pCookie)
+{
+	printf("Gesture %s: Intermediate stage complete (%f,%f,%f)\n", strGesture, pPosition->X, pPosition->Y, pPosition->Z);
+}
+void XN_CALLBACK_TYPE GestureReadyForNextIntermediateStageHandler(xn::GestureGenerator& generator, const XnChar* strGesture, const XnPoint3D* pPosition, void* pCookie)
+{
+	printf("Gesture %s: Ready for next intermediate stage (%f,%f,%f)\n", strGesture, pPosition->X, pPosition->Y, pPosition->Z);
+}
+void XN_CALLBACK_TYPE GestureProgressHandler(xn::GestureGenerator& generator, const XnChar* strGesture, const XnPoint3D* pPosition, XnFloat fProgress, void* pCookie)
+{
+	printf("Gesture %s progress: %f (%f,%f,%f)\n", strGesture, fProgress, pPosition->X, pPosition->Y, pPosition->Z);
+}
 
 
 
