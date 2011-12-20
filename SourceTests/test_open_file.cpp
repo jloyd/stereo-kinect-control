a96939e21c31f90213009ae0b941d5525dbb3276
dj.quam@gmail.com <dj.quam@gmail.com@d7e7eb5c-c029-b68f-3db2-f32950ae7c21>
2011-12-20 16:12:56 +0000

    added test configuration fiels

---
+++ SourceTests/test_open_file.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+
+using namespace System;
+using namespace System::Text;
+using namespace System::Collections::Generic;
+using namespace	Microsoft::VisualStudio::TestTools::UnitTesting;
+
+namespace SourceTests
+{
+	[TestClass]
+	public ref class test_open_file
+	{
+	public: 
+		[TestMethod]
+		void TestMethod1()
+		{
+		}
+	};
+}
