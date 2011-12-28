4320f2b3d61cdce6cdfcb883dda10cd6203d2482
dj.quam@gmail.com <dj.quam@gmail.com@d7e7eb5c-c029-b68f-3db2-f32950ae7c21>
2011-12-28 23:18:55 +0000

    added toggle zoom

--- branches/COMAuto/Subversion_Kinect/stereoplayer_h.h
+++ branches/COMAuto/Subversion_Kinect/stereoplayer_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Wed Dec 28 13:27:41 2011
+/* at Wed Dec 28 16:56:28 2011
  */
 /* Compiler settings for stereoplayer.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
